8796bc5c570980dbd50bd9d0f8b59979930245db
LCassio99 <lucianocassio66@gmail.com>
2023-06-01 19:30:44 -0300

    exercicio1

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -5,3 +5,15 @@ const array = [
     [20, 33, 11, 12, 19],
     [3, 3, 4, 5, 10]
 ]
+  
+  for (let indiceJogadores = 0; indiceJogadores < array.length; indiceJogadores++) {
+    const jogador = array[indiceJogadores]
+    let gols = `Jogador ${indiceJogadores + 1}: `
+  
+    for (let jogadores = 0; jogadores < jogador.length; jogadores++) {
+      gols += `${jogador[jogadores]}, `
+    }
+    gols = gols.slice(0, -2)
+  
+    console.log(gols)
+  }
