ca9b003e043cf66a56de379d2aff018e51636b9b
LCassio99 <lucianocassio66@gmail.com>
2023-06-01 20:31:23 -0300

    correcao exercicio2

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -6,9 +6,9 @@ const tabuada = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 for (let indiceNumero in tabuada){
 
-  const resultado = numero * indiceNumero
+  const resultado = numero * (Number(indiceNumero) + 1)
 
-  if (resultado != 0) console.log(resultado)
+  console.log(resultado)
 
 }
    
